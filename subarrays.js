057c34d63e6ce8f46df0271254c5257e998a2c7e
Robins <robinmgk077@gmail.com>
2024-03-28 17:36:04 +0300

    first commit

---
+++ subarrays.js
@@ -0,0 +1,24 @@
+//given an array and a chunk size, divide the array  into sub arrays where each subarray is of the length the chunk size
+
+//([1,2,3,4,5,6,7]3) == ([1,2,3], [3,4,5], [6,7])
+//([1,2,3,4,5,6,7]1) == ([1][2] [3],[4] [5] [6] [7])
+
+
+//loop through the array
+
+arr = [1, 2, 3, 4, 5, 6, 7];
+size = 2;
+
+function chunkArray(arr, size) {
+  //variable to store the array
+  let chunks = [];
+  //looping through the array and ensuring it has values
+  while (arr.length) {
+    //pushing the subarrays in the enpty chunks array
+    chunks.push(arr.splice(0, size));
+  }
+  return chunks;
+}
+
+console.log(chunkArray(arr, size));
+
